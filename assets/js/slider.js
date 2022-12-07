5b1afb9105d736397a34f3dbf3f221045e0a9f78
Stelian2022 <stanciu.stelian86@yahoo.fr>
2022-12-07 17:03:50 +0100

    MOD

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -2,13 +2,13 @@ function sliderManager(sliderState) {
   const sliderImg = document.querySelector("#slider img");
   console.dir(sliderImg);
 
-
   //on a utiliser setTimeout pour laisser un temps pour le navigateur d'affiché l'image
   setTimeout(() => {
-    let imgHeight = (90 * sliderImg.clientHeight) / sliderImg.clientWidth;
+    let imgHeight = (50 * sliderImg.clientHeight) / sliderImg.clientWidth;
 
     console.log(document.querySelector("#slider img").offsetHeight);
     console.log(imgHeight);
+    document.querySelector("#slider").style.height =imgHeight + "%";
   }, 500);
 
   switch (sliderState) {
